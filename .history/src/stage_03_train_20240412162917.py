d9504706a753161dc80e90d3f1670b593fc7ef61
JIGARKUMAR PARMAR <parmarjigarkumar147@gmail.com>
2024-04-12 16:48:16 +0530

    stage 03 model train added

---
+++ .history/src/stage_03_train_20240412162917.py
@@ -0,0 +1,40 @@
+import os
+from src.utils.all_utils import read_yaml
+import argparse
+import pandas as pd
+from sklearn.linear_model import ElasticNet
+
+def train(config_path,params_path):
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+
+    # save dataset in local directory 
+    artifacts_dir = config['artifacts']['artifacts_dir']
+    split_data_dir = config['artifacts']['split_data_dir']
+
+    train_data_filename = config['artifacts']['train']
+
+    train_data_path = os.path.join(artifacts_dir,split_data_dir,train_data_filename)
+    
+    train_data = pd.read_csv(train_data_path)
+
+    train_y = train_data['quality']
+    train_x = train_data.drop("quality",axis = 1)
+
+    alpha = params['model_params']['ElasticNet']['alpha']
+    l1_rate = params['model_params']['ElasticNet']['l1_rate']
+    random_state = params['base']['random_state']
+
+    lr = ElasticNet(alpha= alpha, l1_ratio=l1_rate, random_state=random_state)
+    lr.fit(train_x,train_y)
+    print('done')
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+
+    args.add_argument("--config","-c",default="config/config.yaml")
+    args.add_argument("--params","-p",default="params.yaml")
+
+    parsed_args = args.parse_args()
+
+    train(config_path = parsed_args.config, params_path = parsed_args.params)
