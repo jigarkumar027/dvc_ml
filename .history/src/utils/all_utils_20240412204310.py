b99cc347f2affb526925def1f91ab62a9eecef3e
JIGARKUMAR PARMAR <parmarjigarkumar147@gmail.com>
2024-04-12 20:56:06 +0530

    added evalauate stage

--- .history/src/utils/all_utils_20240412204310.py
+++ .history/src/utils/all_utils_20240412204310.py
@@ -17,7 +17,7 @@ def save_local_df(data,data_path, index_status = False):
     data.to_csv(data_path,index = index_status)
     print(f"data is saved at {data_path}")
 
-def save_reports(report: dict, report_path: str,indentation=4 ):
+def save_reports(report: dict, report_path: str):
     with open(report_path,'w') as f:
-        json.dump(report,f,indent=indentation)
+        json.dump(report,f,indent=4)
     print(f"report are saved at {report_path}")
