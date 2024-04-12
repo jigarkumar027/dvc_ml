8a1aef6d2091c4bc5e2510c79444b0b8307a929a
JIGARKUMAR PARMAR <parmarjigarkumar147@gmail.com>
2024-04-12 15:47:21 +0530

    stage 02 split data added

---
+++ .history/src/stage_02_split_data_20240412151331.py
@@ -0,0 +1,33 @@
+import os
+from src.utils.all_utils import read_yaml, create_directory
+import argparse
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def split_and_save(config_path,params_path):
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+
+    # save dataset in local directory 
+    artifacts_dir = config['artifacts']['artifacts_dir']
+    raw_local_dir = config['artifacts']['raw_local_dir']
+    raw_local_file = config['artifacts']['raw_local_file']
+
+    raw_local_file_path = os.path.join(artifacts_dir,raw_local_dir,raw_local_file)
+    
+    df = pd.read_csv(raw_local_file_path)
+
+    split_ratio = params['base']['test_size']
+    random_state = params['base']['random_state']
+
+    train, test = train_test_split(df,test_size = ,random_state= )
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+
+    args.add_argument("--config","-c",default="config/config.yaml")
+    args.add_argument("--params","-p",default="params.yaml")
+
+    parsed_args = args.parse_args()
+
+    split_and_save(config_path = parsed_args.config, params_path = parsed_args.params)
